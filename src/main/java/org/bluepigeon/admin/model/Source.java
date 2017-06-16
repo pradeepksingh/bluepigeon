7de623efe1eb496d42a4d7e7b1c3e0a91ece9597
pankaj Naik <pankajnaik2003@gmail.com>
2017-06-16 12:53:31 +0530

    added new model Source

---
+++ src/main/java/org/bluepigeon/admin/model/Source.java
@@ -0,0 +1,36 @@
+package org.bluepigeon.admin.model;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "source", catalog = "blue_pigeon")
+public class Source {
+	
+	private Integer id;
+	private String name;
+	
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "id", unique = true, nullable = false)
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	@Column(name = "name")
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
